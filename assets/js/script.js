43a694ed69b2999ed3e0ae8c0f654bf21fac22da
Stephanie Mentzer <43530863+smentzer@users.noreply.github.com>
2021-09-29 16:28:23 -0400

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,20 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var elems = document.querySelectorAll(".sidenav");
+    var instances = M.Sidenav.init(elems);
+  });
+
+    //drop down trigger
+    $(".dropdown-trigger").dropdown();
+
+    //parallax
+    $(document).ready(function(){
+        $('.parallax').parallax();
+      });
+
+
+      $(document).ready(function(){
+        $('.materialboxed').materialbox();
+      });
+
+
+      
